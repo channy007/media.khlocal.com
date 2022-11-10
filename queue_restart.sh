29ee48e186a5563811c5d7cbd8aff4810d3d116e
LAPTOP-9G63GLH8\USER <onepower.op@gmail.com>
2022-11-10 10:16:56 +0700

    Added restart queue file

---
+++ queue_restart.sh
@@ -0,0 +1,8 @@
+## Stop process
+sudo supervisorctl stop video-downloader-queue:*
+## Reread process
+sudo supervisorctl reread video-downloader-queue:*
+## Update process
+sudo supervisorctl update video-downloader-queue:*
+## Start process
+sudo supervisorctl start video-downloader-queue:*
