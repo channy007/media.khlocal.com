406766ee2a0b46db7b070d011af6c3766901f4fe
LAPTOP-9G63GLH8\USER <onepower.op@gmail.com>
2022-11-18 00:36:04 +0700

    Added resumalbe upload script

---
+++ public/shell_scripts/bk/ffmpeg_cut_bk_1.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#usage: ./ffmpeg.sh filename transition seg_start seg_length seg_gap fliph flipv
+#new usage: ./ffmpeg.sh filename transition seg_start seg_length seg_gap flip
+
+
+file="$1"
+transition="$2"
+file_name="${file%.*}"
+extension="${file##*.}"
+video_length=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${file}")
+seg_start="$3"
+length="$4"
+gap="$5"
+flip="$6"
+
+if [[ -v flip ]];
+then
+    echo "variable named a is already set"
+else
+    echo "variable a is not set"
+    flip=""
+fi
+##################################################################################################################
+rm -rf "${file_name}"
+mkdir "${file_name}"
+clear -x
+##################################################################################################################
+start_time=$(/bin/date +%s)
+##################################################################################################################
+seg_gap=$seg_start
+seg_length=$(( $RANDOM%$length+1 ))
+seg_end=$(( $seg_start+$seg_length ))
+fade_duration=1
+fade_prev=0
+fade_next=1
+all_duration=0
+while (($(echo "$video_length >= $seg_end" | bc))); do
+        all_video_fade="$all_video_fade$video_fade"
+        all_audio_fade="$all_audio_fade$audio_fade"
+        inputs="$inputs -i $file "
+
+	speed=$(bc -l <<< "1 - $(( $RANDOM%5+5 )) / 100")
+        all_duration=$(bc -l <<< "$all_duration + $seg_length * $speed")
+        offset=$(bc -l <<< "$all_duration - $fade_duration * $fade_next")
+	
+	video_scale="$video_scale[$fade_prev:v]trim=start=$seg_start:end=$seg_end,crop=ih*4/3:ih,scale=640:480,setpts=$speed*(PTS-STARTPTS),hflip[v$fade_prev];"
+        video_fade="[vfade$fade_prev][v$fade_next]xfade=transition=$transition:duration=$fade_duration:offset=$offset[vfade$fade_next];"
+
+	audio_scale="$audio_scale[$fade_prev:a]atrim=start=$seg_start:end=$seg_end,asetpts=PTS-STARTPTS,atempo=1/$speed[a$fade_prev];"
+        audio_fade="[afade$fade_prev][a$fade_next]acrossfade=d=$fade_duration[afade$fade_next];"
+
+	seg_length=$(( $RANDOM%$length+1 ))
+	seg_gap=$(( $RANDOM%$gap+1 ))
+        seg_start=$(( $seg_end+$seg_gap ))
+        seg_end=$(( $seg_start+$seg_length ))
+        (( fade_prev++ ))
+        (( fade_next++ ))
+done
+fade_prev=$(( fade_prev - 1 ))
+all_video_fade="[v0]copy[vfade0];$all_video_fade[vfade$fade_prev]format=yuv420p"
+all_audio_fade="[a0]acopy[afade0];$all_audio_fade[afade$fade_prev]acopy"
+set -vx
+ffmpeg -y -hide_banner $inputs \
+        -filter_complex "$video_scale$all_video_fade;$audio_scale$all_audio_fade" \
+        -movflags +faststart \
+        "${file_name}_cut.${extension}" > /dev/null 2>&1
+##################################################################################################################
+end_time=$(/bin/date +%s)
+elapsed=$((end_time - start_time))
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
---
+++ public/shell_scripts/resumable_upload_fb.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#usage facebook_page(1) access_token(2) source(3)(full path source file) description(4) title(5) thumbnail(6) 
+
+##### capture arguments
+facebook_link="https://graph.facebook.com/v15.0"
+facebook_page_id="$1"
+access_token="$2"
+description="description=$4"
+title="title=$5"
+thumb="$6"
+if [[ -z "$thumb" ]];
+then
+    echo "video thumbnail has been not set"
+    thumb="test=''"
+else
+    echo "video thumbnail has been set"
+    thumb="thumb=@$6"
+fi
+
+file="$3"
+filename="${file%.*}"
+file_extension="${file##*.}"
+filesize=$(find "$filename.$file_extension" -printf "%s")
+printf "File size = %d\n" $filesize
+###### end capture argurment
+
+##### static arguments
+
+# access_token="EAASNg0JLXNgBAMbYUFpmvaFakgRzJSJ07iZBF1PN1IL9ISM3kpa2yV7W9gZAPAjLm4SjRLWg74xbZBUXMs0NGoS49Gz9MFFQKVjfpShXlqMWw1mm5WPDIkmqQZCIj5QM3TgXpCAyNxsZCyZCgpBNq0nZBnc6bmm7zVfTAF2zw2PTcbrneAak2dw"
+# facebook_page_id="102318056026253"
+
+##### end static argument
+
+split_video(){
+    #create folder for split file
+    mkdir -p $filename
+
+    #split file
+    split -b 25485760 "$filename.$file_extension" "$filename/"
+}
+
+init_facebook_seasion(){
+    output=$(curl -X POST \
+    "https://graph-video.facebook.com/v15.0/$facebook_page_id/videos" \
+    -F "upload_phase=start" \
+    -F "access_token=$access_token" \
+    -F "file_size=$filesize" 2>/dev/null)
+
+    output=$(echo $output | jq )
+
+    echo "response json: $output"
+    ##
+    # Capture the response into variable
+    ##
+    video_id=$(echo $output | jq .video_id)
+    start_offset=$(echo $output | jq .start_offset)
+    end_offset=$(echo $output | jq .end_offset)
+    upload_session_id=$(echo $output | jq .upload_session_id)
+
+    echo "
+    Upload result: 
+    video_id: $video_id,
+    start_offset: $start_offset,
+    end_offset: $end_offset,
+    upload_session_id: $upload_session_id
+    "
+}
+
+upload_chunk_to_facebook(){
+    video_chuck="$1"
+    upload_response=$(curl -X POST \
+    "https://graph-video.facebook.com/v15.0/$facebook_page_id/videos"  \
+    -F "upload_phase=transfer" \
+    -F "upload_session_id=$upload_session_id" \
+    -F "access_token=$access_token" \
+    -F "start_offset=$start_offset" \
+    -F "video_file_chunk=@$video_chuck")
+
+    ##
+    # Capture the response into variable
+    ##
+    start_offset=$(echo $output | jq .start_offset)
+}
+
+upload_end_session(){
+    curl -X POST \
+    "https://graph-video.facebook.com/v15.0/$facebook_page_id/videos"  \
+    -F "upload_phase=finish" \
+    -F "access_token=$access_token" \
+    -F "upload_session_id=$upload_session_id" \
+    -F "$description" \
+    -F "$title" \
+    -F "$thumb"
+}
+
+split_video
+
+init_facebook_seasion
+
+## start upload all video's chunks to facebook
+for file_path in "$filename/*";
+    do 
+        upload_chunk_to_facebook $file_path
+    done
+
+## tell facebook the end of upload session
+upload_end_session
