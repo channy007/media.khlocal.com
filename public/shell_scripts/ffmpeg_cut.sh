58d30ad362c78b8e663d74fefab3ff7e669d91a1
LAPTOP-9G63GLH8\USER <onepower.op@gmail.com>
2022-11-18 18:11:27 +0700

    Fixed error script

--- public/shell_scripts/ffmpeg_cut.sh
+++ public/shell_scripts/ffmpeg_cut.sh
@@ -26,11 +26,14 @@ else
 fi
 
 ## finding scal base on resolution
-if [[ $resolution -eq '1:1' ]] then
+if [[ "$resolution" -eq "1:1" ]];
+then
         scale="crop=ih:ih,scale=1080:1080"
-elif [[ $resolution -eq '4:3']] then
+elif [[ $resolution -eq "4:3"]];
+then
         scale="crop=ih*4/3:ih,scale=640:480" 
-elif [[ $resolution -eq '16:9' ]] then
+elif [[ $resolution -eq "16:9" ]];
+then
         scale="crop=ih*16/9:ih,scale=1280:720"
 else
         scale="crop=ih*4/3:ih,scale=640:480" 
