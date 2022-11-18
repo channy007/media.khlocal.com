1c9fa50f64ee72794e76a81e663e4938dc15afbe
LAPTOP-9G63GLH8\USER <onepower.op@gmail.com>
2022-11-18 09:03:41 +0700

    decrement timeout for default horizon

--- config/horizon.php
+++ config/horizon.php
@@ -174,7 +174,7 @@ return [
             'maxJobs' => 0,
             'memory' => 128,
             'tries' => 1,
-            'timeout' => 12600,
+            'timeout' => 3600,
             'nice' => 0,
         ],
         'supervisor-long-process' => [
