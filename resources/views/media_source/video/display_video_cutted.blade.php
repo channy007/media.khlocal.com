4d90f4426b2d89b38ee626b45749f81b91cf5ce1
DEVELOPER\voeun <onepower.op@gmail.com>
2022-11-25 12:21:50 +0700

    Update video video downloaded

--- resources/views/media_source/video/display_video_cutted.blade.php
+++ resources/views/media_source/video/display_video_cutted.blade.php
@@ -8,8 +8,8 @@
 
 <body>
 
-    <div class="container" style="display: flex;justify-content: center; max-width: 100%;max-height: 750px;">
-        <video controls="" autoplay="" name="media">
+    <div style="display: flex;justify-content: center;max-height: 100%;;max-width: 100%;">
+        <video controls="" autoplay="" name="media" style="width: 885px;height: 498px;">
             <source src="{{ $data->play_url.'/'.$data->name_cutted.'.'.$data->extension }}"
                 type="video/mp4">
         </video>
