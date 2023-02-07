d926c88c40926892a310ad59822f1aafdd8aaf45
DEVELOPER\voeun <onepower.op@gmail.com>
2023-02-08 01:33:52 +0700

    Fixed auto download

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('youtbue:auto-download')->at("23:00")->timezone('Asia/Phnom_Penh');
+        $schedule->command('youtube:auto-download')->at("23:00")->timezone('Asia/Phnom_Penh');
     }
 
     /**
